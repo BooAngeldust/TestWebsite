f4bf67fa22ea5db8cff176fc53882b268b8136be
BooAngeldust <alex241001@outlook.com>
2022-01-07 13:07:05 +0000

    Initial commit

---
+++ src/View/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="../Style/stylesheet.css">
+</head>
+<body>
+	<div class="header">
+		<h1>My Website</h1>
+		<p>A very <b>brief</b> description.</p>
+	</div>
+
+	<div class="navbar">
+		<a href="index.php">Home</a>
+		<a href="about.php">About</a>
+	</div>
---
+++ src/View/index.php
@@ -0,0 +1,41 @@
+
+<?php
+	$title = "Homepage";
+
+	include('header.php');
+?>
+
+<!-- Content -->
+<div class="content">
+	<div class="boss-description">
+		<h3>Nameless</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie odio sed purus elementum, id vestibulum lectus dictum. Phasellus pellentesque elit ligula, in rhoncus massa posuere non. Maecenas vitae quam aliquam lacus hendrerit pellentesque. Nullam rhoncus commodo pharetra. Vivamus tincidunt ac leo quis tempor. Quisque aliquam arcu sapien, tincidunt auctor turpis pretium ut. Vestibulum ornare interdum nunc, quis sagittis velit blandit sed. Donec sit amet placerat mauris, ut vehicula tellus. Phasellus ut malesuada ipsum. Mauris quis orci a felis molestie condimentum. In rutrum augue erat, vitae elementum nibh egestas sit amet. Mauris 
+			mattis est convallis dolor cursus, id consectetur odio varius.
+		</p> 
+		<img src="https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/namelessking2_tc.jpg" width="600" height="400">
+	</div>
+
+	<div class="boss-description">
+		<h3>Gael</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie odio sed purus elementum, id vestibulum lectus dictum. Phasellus pellentesque elit ligula, in rhoncus massa posuere non. Maecenas vitae quam aliquam lacus hendrerit pellentesque. Nullam rhoncus commodo pharetra. Vivamus tincidunt ac leo quis tempor. Quisque aliquam arcu sapien, tincidunt auctor turpis pretium ut. Vestibulum ornare interdum nunc, quis sagittis velit blandit sed. Donec sit amet placerat mauris, ut vehicula tellus. Phasellus ut malesuada ipsum. Mauris quis orci a felis molestie condimentum. In rutrum augue erat, vitae elementum nibh egestas sit amet. Mauris 
+			mattis est convallis dolor cursus, id consectetur odio varius.
+		</p> 
+		<img src="https://i.ytimg.com/vi/U2BVfbZ2WMg/maxresdefault.jpg" width="600" height="400">
+	</div>
+
+	<div class="boss-description">
+		<h3>Abyss</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie odio sed purus elementum, id vestibulum lectus dictum. Phasellus pellentesque elit ligula, in rhoncus massa posuere non. Maecenas vitae quam aliquam lacus hendrerit pellentesque. Nullam rhoncus commodo pharetra. Vivamus tincidunt ac leo quis tempor. Quisque aliquam arcu sapien, tincidunt auctor turpis pretium ut. Vestibulum ornare interdum nunc, quis sagittis velit blandit sed. Donec sit amet placerat mauris, ut vehicula tellus. Phasellus ut malesuada ipsum. Mauris quis orci a felis molestie condimentum. In rutrum augue erat, vitae elementum nibh egestas sit amet. Mauris 
+			mattis est convallis dolor cursus, id consectetur odio varius.
+		</p> 
+		<img src="https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/abyss-watchers-dks3.jpg" width="600" height="400">
+	</div>
+</div>
+
+<?php
+	include('footer.php')
+?>
+	
