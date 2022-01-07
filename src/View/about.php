7c45bb4781d6ecb502379e6d7007d9aef313080e
BooAngeldust <alex241001@outlook.com>
2022-01-07 14:13:24 +0000

    Fixed footer

--- src/View/about.php
+++ src/View/about.php
@@ -5,11 +5,12 @@
 ?>
 
 <!-- Content -->
+<div class="content">
 <h2>About</h2>
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget enim et justo rhoncus rhoncus. Vestibulum tincidunt arcu ut lectus hendrerit, non scelerisque urna tempor. Etiam hendrerit augue mi, lobortis semper lacus venenatis interdum. Pellentesque et vestibulum felis, in lobortis neque. Nunc maximus mi id fermentum lobortis. Vivamus non dui id dolor finibus accumsan. Aliquam ut placerat leo. Aenean non neque id nisl dictum dignissim eu ut turpis. Donec suscipit lorem mi, nec facilisis tellus accumsan pellentesque. Nam rhoncus tortor sem, eget malesuada nulla semper vitae. Pellentesque bibendum velit quis maximus imperdiet. Maecenas urna lacus, tincidunt sit amet tincidunt placerat, vestibulum porta enim.
 </p>
-
+</div>
 <?php
 	include('footer.php')
 ?>
